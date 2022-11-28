cb1a4cbc9ee3edb2f632ac3d4cf02b896c1ebd9b
MIKITA IODA <nikitaiodo201@gmail.com>
2022-11-28 09:05:41 +0300

    one line delete in Main

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Введите пароль для входа в систему.");
-        System.out.println("Только аккуратно:)");
         int value = new Scanner(System.in).nextInt();
         Security security = new Security();
         security.Check(value);
