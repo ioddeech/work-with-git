b551c05b10007b11a0c1efd20d1b3b59a60b4ea7
MIKITA IODA <nikitaiodo201@gmail.com>
2022-11-28 09:00:18 +0300

    +1 line

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите код для входа в систему.");
+        System.out.println("Введите пароль для входа в систему.");
+        System.out.println("Только аккуратно:)");
         int value = new Scanner(System.in).nextInt();
         Security security = new Security();
         security.Check(value);
