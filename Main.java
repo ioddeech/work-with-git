a4f6574611c57d37e81f57169978e2c90a26fcb6
MIKITA IODA <nikitaiodo201@gmail.com>
2022-11-29 09:31:28 +0300

    add generation password

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
+import java.awt.print.PrinterGraphics;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Введите пароль для входа в систему.");
-        int value = new Scanner(System.in).nextInt();
-        Security security = new Security();
-        security.Check(value);
+        int value2 = new Random().nextInt(10000);
+        while (true) {
+            System.out.println("Введите пароль для входа в систему.");
+            int value = new Scanner(System.in).nextInt();
+            Security security = new Security();
+            security.Check(value, value2);
+        }
+
     }
 }
--- Security.java
+++ Security.java
@@ -1,11 +1,18 @@
 import javax.swing.*;
+import java.util.Random;
 
 public class Security {
-    public void Check(int code) {
-        if (code == 1234) {
-            System.out.println("Проход открыт.");
-        } else {
-            System.out.println("Неверный пароль.");
-        }
+
+    public void Check(int code, int rightCode) {
+        //while (true) {
+            if (code == rightCode) {
+                System.out.println("Проход открыт.");
+
+            } else if (code == 1212) {
+                System.out.println("Пароль равен: " + rightCode);
+            } else {
+                System.out.println("Неверный пароль.");
+            }
+       // }
     }
 }
