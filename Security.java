a8772d3f3aecc1b129ad139ce9e1b29a1c7655dd
MIKITA IODA <nikitaiodo201@gmail.com>
2022-11-29 09:50:00 +0300

    gay check

--- Security.java
+++ Security.java
@@ -1,11 +1,39 @@
 import javax.swing.*;
+import java.util.Scanner;
 
 public class Security {
+    public boolean boo;
+
     public void Check(int code) {
+        CheckOrientation();
+        if (boo == false) return;
         if (code == 1234) {
             System.out.println("Проход открыт.");
         } else {
             System.out.println("Неверный пароль.");
         }
     }
+
+    public boolean CheckOrientation() {
+        System.out.println("Вы гей?");
+        System.out.println("1. Yes" + "\n" +
+                "2. Нет");
+        int thb = new Scanner(System.in).nextInt();
+        boo = true;
+
+        while (boo == true) {
+            if (thb == 1) {
+                System.out.println("Отлично!!!");
+                return boo;
+            } else if (thb == 2) {
+                System.out.println("Вы не подходите");
+                return boo = false;
+            } else {
+                System.out.println("Введено некорректное значение.");
+            }
+        }
+        return boo = true;
+    }
+
 }
+
