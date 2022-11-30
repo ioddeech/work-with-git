013e99949fe28fd0a4e7a591eea52b74b73409d4
MIKITA IODA <nikitaiodo201@gmail.com>
2022-11-30 11:07:14 +0300

    -2 lines

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
+import com.sun.org.apache.xalan.internal.xsltc.trax.XSLTCSource;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Введите код для входа.");
+
+
+        System.out.println("Только аккуратно)");
 
         int value = new Scanner(System.in).nextInt();
 
