a773ea502700aec0db0f2f5a9622e7eb307bc95b
MIKITA IODA <nikitaiodo201@gmail.com>
2022-11-30 11:04:44 +0300

    па приколу2

--- Security.java
+++ Security.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Security {
     public void Check(int code) {
         if (code == 1234) {
-            System.out.println("Проход открыт.");
+            System.out.println("Проход открыт, можете пройти.");
         } else {
             System.out.println("Неверный код доступа.");
         }
