0b2f2cd517606d6dd2397730fd95dca6a37a2151
MIKITA IODA <nikitaiodo201@gmail.com>
2022-12-01 09:32:58 +0300

    delete the empty line

--- Main.java
+++ Main.java
@@ -4,17 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Введите код для входа.");
-
-
         System.out.println("Только аккуратно)");
-
         int value = new Scanner(System.in).nextInt();
-
         Security security = new Security();
-
         security.Check(value);
-
     }
 }
